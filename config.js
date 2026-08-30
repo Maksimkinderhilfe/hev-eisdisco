ae2a7eb6e9d29b061c40bb1dc5b3d97a7a4d256b
DJ BREEZE <maksimno1912@gmail.com>
2026-08-30 19:46:46 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase connection for HEV EISDISCO
+window.HEV_CONFIG = {
+  SUPABASE_URL: "https://onpeujlgcgiqxdxjebsb.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_h-x_oTS33T3maxhBjdZWLg_tO8CKhf8"
+};
